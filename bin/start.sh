61fe07cda11a696ec31dcc03bd7adff066b1c7c3
芦厚祥 <houxiang.lu@inin88.com>
2017-11-22 20:50:02 +0800

    Feat: 1、增加状态查询代码，修改删除程序的代码, 2、网页显示内容时随着内容扩展编辑框的大小

--- bin/start.sh
+++ bin/start.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 nohup /opt/kds/anaconda3/envs/python3.5.3/bin/python /opt/kds/mobile-stock/UnitTestServer/manage.py runserver 0.0.0.0:8000 >/dev/null 2>log  &
+PIDS=$!
+echo $PIDS
+echo $PIDS > UnitTestServer.pid
--- bin/stop.sh
+++ bin/stop.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-`./.stop.sh > /dev/null 2> log`
+#`./.stop.sh > /dev/null 2> log`
+pid=`cat UnitTestServer.pid` 
+echo $pid
+
+for p_pid in `ps -ef |grep $pid|grep -v grep | awk '{print $2}'`
+do
+	echo $p_pid
+	pkill -9 -P $p_pid
+done
