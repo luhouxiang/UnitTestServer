89d8f4a0c9b68eedf84088a88f740b21fc51c525
芦厚祥 <houxiang.lu@inin88.com>
2017-11-23 10:54:27 +0800

    Feat: 增加启动，停止，进程结果检测判断

--- bin/start.sh
+++ bin/start.sh
@@ -3,5 +3,9 @@ export PYTHON_HOME=/opt/kds/anaconda3/envs/python3.5.3
 export SERVER_HOME=/opt/kds/mobile-stock/UnitTestServer
 nohup $PYTHON_HOME/bin/python $SERVER_HOME/manage.py runserver 0.0.0.0:8000 >/dev/null 2>log  &
 PIDS=$!
-echo $PIDS
 echo $PIDS > $SERVER_HOME/bin/UnitTestServer.pid
+
+if [ `ps -fP $PIDS | wc -l` -gt 1 ]
+then echo "UnitTestServer: 服务已启动"
+else echo "UnitTestServer: 服务启动失败"
+fi
--- bin/status.sh
+++ bin/status.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-
+echo python协议测试UnitTestServer服务：
 pid=`cat UnitTestServer.pid`
 ps -ef | grep -v grep | grep $pid
