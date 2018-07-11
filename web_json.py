43047305697039bc46742818570c511d6d05570d
芦厚祥 <luhouxiang@btktrade.com>
2018-07-11 23:16:16 +0800

    Feat: 解决url直接post不成功的问题

--- web_json.py
+++ web_json.py
@@ -14,22 +14,37 @@
 # data = '{"name": "jack", "id": "1"}'
 # web.post(params, data)
 
-from urllib.request import urlopen
+# from urllib.request import urlopen
 import json
 import ssl
+import requests
 
 class WebJson(object):
     def __init__(self):
         pass
 
     @classmethod
-    def get_url_json(cls, url, data):
+    def get_url_json(cls, url, data, method):
         ssl._create_default_https_context = ssl._create_unverified_context # 解决https的认证问题
-        web = urlopen(url, data)
-        print(web.url)
-        print("status: ", web.status)
-        raw_text = web.read()
-        json_data = json.loads(raw_text.decode('utf8'))
+        response = None
+        if method == "GET":
+            response = requests.get(url)
+        else:
+            response = requests.post(url, data)
+
+        print(url)
+        print("status: ", response.status_code)
+        res_text = response.content.decode()
+        json_data = None
+        try:
+            json_data = json.loads(res_text)
+        except:
+            json_data = res_text
+        # web = urlopen(url, data)
+        # print(web.url)
+        # print("status: ", web.status)
+        # raw_text = web.read()
+        # json_data = json.loads(raw_text.decode('utf8'))
         return json_data
 
     @classmethod
@@ -38,25 +53,25 @@ class WebJson(object):
         return json_text
 
     @classmethod
-    def get_url_data(cls, url, data):
+    def get_url_data(cls, url, data, method):
         """
         :param url:
         :param data:
         :return:
         """
-        json_data = cls.get_url_json(url, data)
+        json_data = cls.get_url_json(url, data, method)
         json_text = cls.parse_json(json_data)
         print(json_text)
         return json_text
 
     @classmethod
     def get(cls, url):
-        return cls.get_url_data(url, None)
+        return cls.get_url_data(url, None, "GET")
     
     @classmethod
     def post(cls, url, data):
         data = bytes(data, 'utf8')
-        return cls.get_url_data(url, data)
+        return cls.get_url_data(url, data, "POST")
 
 
 if __name__ == '__main__':
