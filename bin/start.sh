64e076067d4dc6cc5d9652873e67390331718c48
芦厚祥 <luhouxiang@btktrade.com>
2018-07-11 11:55:54 +0800

    Feat: 修改启动脚本

--- bin/start.sh
+++ bin/start.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-export PYTHON_HOME=/opt/kds/anaconda3/envs/python3.5.3
-export SERVER_HOME=/opt/kds/mobile-stock/UnitTestServer
-nohup $PYTHON_HOME/bin/python $SERVER_HOME/manage.py runserver 0.0.0.0:8000 >/dev/null 2>log  &
+# export PYTHON_HOME=/opt/kds/anaconda3/envs/python3.5.3
+# export SERVER_HOME=/opt/kds/mobile-stock/UnitTestServer
+nohup python ../manage.py runserver 0.0.0.0:8000 >/dev/null 2>log  &
 PIDS=$!
-echo $PIDS > $SERVER_HOME/bin/UnitTestServer.pid
+echo $PIDS > UnitTestServer.pid
 
 if [ `ps -fP $PIDS | wc -l` -gt 1 ]
 then echo "UnitTestServer: 服务已启动"
